56a90ee9f342dcf597bef1774eed7860d692222e
Rishika Chopara <rishika.chopara@gmail.com>
2017-12-12 14:14:08 +1300

    split() didn't work - removed. Finished off the rest of the function.

--- Summarising Function.R
+++ Summarising Function.R
@@ -7,31 +7,54 @@ summarise = function(results) {
   # The names of the variables that have been monitored in the model-fitting funciton
   names = names(results[1,])
   # If "s" and "z" have not been monitored in the model-fitting function
-  if (length(grep("s[^i]", names)) == 0 & length(grep("z", names)) == 0) {list = split(results, names)}
+  if (length(grep("s[^i]", names)) == 0 & length(grep("z", names)) == 0) {results.2 = results}
   # If "s" and "z" have been monitored in the model-fitting function
   if (length(grep("s[^i]", names)) != 0 & length(grep("z", names)) != 0) {
     results.2 = results[,-c(grep("s[^i]", names), grep("z", names))]
     names.2 = names[-c(grep("s[^i]", names), grep("z", names))]
-    list = split(results.2, names.2)
   }
   # If "s" has been monitored and "z" has not
   if (length(grep("s[^i]", names)) != 0 & length(grep("z", names)) == 0) {
     results.2 = results[,-c(grep("s[^i]", names))]
     names.2 = names[-c(grep("s[^i]", names))]
-    list = split(results.2, names.2)
   }
   # If "z" has been monitored and "s" has not
   if (length(grep("s[^i]", names)) == 0 & length(grep("z", names)) != 0) {
     results.2 = results[,-c(grep("z", names))]
     names.2 = names[-c(grep("z", names))]
-    list = split(results.2, names.2)
   }
   
-  names(list)
+  
+  ## Numeric summaries
+  # Means
+  means = apply(results.2, 2, mean)
+  # Standard deviations
+  sdeviations = apply(results.2, 2, sd)
+  
+  # Creating function so can find medians
+  median = function(x) {
+    sorted = sort(x)
+    median = sorted[0.5*length(x)]
+  }
+  # Medians
+  medians = apply(results.2, 2, median)
   
   
+  list = list(means=means, standard.deviations=sdeviations, medians=medians)
   
   
+  ## Histograms
+  plot.new()
+  par(mfrow = c(length(names.2), 1))
+  par(mar = c(2, 0, 1, 0))
+  for (i in 1:length(names.2)) {
+    hist(results.2[,i], breaks=100, main=names.2[i])
+  }
+  
+  return(list)
 }
 
+## Test
+summarised = summarise(results)
+summarised
 
