1da6991bd74d1efb93b92d3e64a05d7d99e3b5fa
Rishika Chopara <rishika.chopara@gmail.com>
2018-01-09 13:27:42 +1300

    Created trace function.

---
+++ Trace Function.R
@@ -0,0 +1,20 @@
+## When writing function - using results from uneven trap grid - need to resolve error with even trap grid!
+
+trace = function(results, par=NULL, s=FALSE, s.no=NULL) {
+  if (s) {
+    val.1 = paste("s", "[", s.no, ",1]", sep="")
+    val.2 = paste("s", "[", s.no, ",2]", sep="")
+    plot(as.numeric(results[,val.1]), as.numeric(results[,val.2]))
+  } else {
+    plot(results[, par])
+  }
+}
+
+
+## TEST
+trace(results=results, "D")
+trace(results=results, s=TRUE, s.no=5)
+
+## NOTE - are the trace plots created for the activity centres what was required? They involve both the x- and y-
+# co-ordinates - are basically scatter plots covering all 10000 iterations of the MCMC. 
+## Should the plot be 3-D?
