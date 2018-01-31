9c32e0434b843b110343f8790922a325a0a88b53
Rishika Chopara <rishika.chopara@gmail.com>
2018-01-31 16:35:47 +1300

    Cleaning up the file

--- Test File.R
+++ Test File.R
@@ -1,18 +1,28 @@
 ### TESTS
 
-### Simulation Function
-## Creating even grid of traps
+## PACKAGES TO LOAD
+# Spatstat package - for grid points
 install.packages("spatstat")
 library("spatstat")
+# AHMBook package - for e2dist function (distance matrices)
+install.packages("AHMBook")
+library("AHMBook")
+# Rjags package - to run MCMC!
+install.packages("rjags")
+library("rjags")
+
+
+### Simulation Function
+## Creating even grid of traps
 window = owin(xrange=c(0,100), yrange=c(0,100))
 points = gridcentres(window, 5, 5)
 traps = as.matrix(cbind(points$x, points$y))
+# Simulating data
+data = simul.data(traps=traps, D=50, buffer=50, g0=0.9, sigma=10, n.occassions=2, seed=2017, discard0=TRUE)
 
 
 ### Model-Fitting Function
-## Even trap grid
-# Smaller D - means that N remains under 500!
-data = simul.data(traps=traps, D=50, buffer=50, g0=0.9, sigma=10, n.occassions=2, seed=2017, discard0=TRUE)
+# NOTE - smaller D - means that N remains under 500 (so can still use M=500)!
 results = fit.model(data=data, M=500, parameters = c("g0", "coeff", "sigma", "N", "D", "z", "s"), n.iter=10000, g0.start = 0.5, log_coeff.start = -5)
 
 
@@ -31,6 +41,6 @@ trace(results=results, s=TRUE, s.no=5)
 
 
 ### Locations Function
-locations(results=results, s.no=1, M=500, xlim=data$xlim, ylim=data$ylim)
+locations(results=results, s.no=77, M=500, xlim=data$xlim, ylim=data$ylim)
 
 
